d1dcceef910302b5ee7e9e199efad65f50f35e07
degrogit <143990248+degrogit@users.noreply.github.com>
2023-09-12 20:31:39 +0300

    Add files via upload

---
+++ Lab1.java
@@ -0,0 +1,8 @@
+public class Lab1 {
+    public static void main(String[] args) {
+        System.out.println("It works!");
+
+        // Додано новий вивід тексту
+        System.out.println("This is a new line.");
+    }
+}
---
+++ lab2.java
@@ -0,0 +1,29 @@
+public class lab2 {
+    public static void main(String[] args) {
+        int levels = 6; // Задайте кількість рівнів ялинки тут
+        
+        printSymmetricTree(levels);
+    }
+    
+    public static void printSymmetricTree(int levels) {
+        for (int i = 1; i <= levels; i++) {
+            // Виводимо пробіли для вирівнювання
+            for (int j = 0; j < levels - i; j++) {
+                System.out.print(" ");
+            }
+            
+            // Виводимо '*' для поточного рівня (спереду)
+            for (int k = 0; k < i; k++) {
+                System.out.print("*");
+            }
+            
+            // Виводимо '*' для поточного рівня (ззаду)
+            for (int k = i - 1; k > 0; k--) {
+                System.out.print("*");
+            }
+            
+            // Перехід на новий рівень
+            System.out.println();
+        }
+    }
+}
